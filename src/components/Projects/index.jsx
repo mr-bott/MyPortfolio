f9db8e00f9bc0355ed1e571db4d62f4fb3d06d58
murali <muralikrishna8309@gmail.com>
2024-10-11 20:17:46 +0530

    projects-croll-2s

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -4,102 +4,102 @@ import React from 'react';
 import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './projects.css'; 
+import './projects.css';
 
 const Projects = () => {
     const fullstackprojects = [
         {
-            name: "Hr Connect", 
+            name: "Hr Connect",
             image: "/assets/fullstack/HrConnect.png",
             link: "https://hr-connect-full-website.vercel.app"
         },
         {
-            name: "Hr Connect", 
+            name: "Hr Connect",
             image: "/assets/fullstack/HrConnect.png",
             link: "https://hr-connect-full-website.vercel.app"
         },
         {
-            name: "Hr Connect", 
+            name: "Hr Connect",
             image: "/assets/fullstack/HrConnect.png",
             link: "https://hr-connect-full-website.vercel.app"
         },
         {
-            name: "Hr Connect", 
+            name: "Hr Connect",
             image: "/assets/fullstack/HrConnect.png",
             link: "https://hr-connect-full-website.vercel.app"
         },
-        
+
     ];
     const froentendprojects = [
         {
-            name: "NxtTrendz", 
+            name: "NxtTrendz",
             image: "/assets/froentend/NxtTrendz.png",
             link: "https://murlinxtrendz.ccbp.tech/"
         },
         {
-            name: "My CCBP Time Line", 
+            name: "My CCBP Time Line",
             image: "/assets/froentend/TimeLine.png",
             link: "https://murliccbptmline.ccbp.tech/"
         },
         {
-            name: "FindJobs", 
+            name: "FindJobs",
             image: "/assets/froentend/FindJobs.png",
             link: "https://muraijobyapp.ccbp.tech/"
         },
         {
-            name: "Password Manager", 
+            name: "Password Manager",
             image: "/assets/froentend/Password.png",
             link: "https://pamana.ccbp.tech/"
         },
         {
-            name: "Stock Marcket", 
+            name: "Stock Marcket",
             image: "/assets/froentend/StockMarcket.png",
             link: "https://stock-marcket-conversly-ai.vercel.app/"
         },
         {
-            name: "3X3 Matrix ", 
+            name: "3X3 Matrix ",
             image: "/assets/froentend/3X3Matrix.png",
             link: "https://3x3-matrix-conversly.vercel.app/"
         },
         {
-            name: "TextAnalysis", 
+            name: "TextAnalysis",
             image: "/assets/froentend/TextAnlysis.png",
             link: "https://text-experiment.vercel.app/"
         },
 
     ]
 
-    const basicprojects=[
+    const basicprojects = [
         {
-        name: "Co2 Emission Calc", 
-        image: "/assets/Basic/Eco.png",
-        link: "https://ecoscore.ccbp.tech/"
-    },
-    {
-        name: "Responsive app", 
-        image: "/assets/Basic/Resmk.png",
-        link: "https://resmk.ccbp.tech/"
-    },
-    {
-        name: "Todo App", 
-        image: "/assets/Basic/Todoapp.png",
-        link: "https://mytdo.ccbp.tech/"
-    },
-    {
-        name: "Cloud Words", 
-        image: "/assets/Basic/CloudWord.png",
-        link: "https://clouf.ccbp.tech/"
-    },
-    {
-        name: "Wiki Search", 
-        image: "/assets/Basic/Wikisearch.png",
-        link: "https://wikili.ccbp.tech/"
-    },{
-        name: "My first website", 
-        image: "/assets/Basic/Ktmm.png",
-        link: "https://ktmm.ccbp.tech/"
-    },
-   
+            name: "Co2 Emission Calc",
+            image: "/assets/Basic/Eco.png",
+            link: "https://ecoscore.ccbp.tech/"
+        },
+        {
+            name: "Responsive app",
+            image: "/assets/Basic/Resmk.png",
+            link: "https://resmk.ccbp.tech/"
+        },
+        {
+            name: "Todo App",
+            image: "/assets/Basic/Todoapp.png",
+            link: "https://mytdo.ccbp.tech/"
+        },
+        {
+            name: "Cloud Words",
+            image: "/assets/Basic/CloudWord.png",
+            link: "https://clouf.ccbp.tech/"
+        },
+        {
+            name: "Wiki Search",
+            image: "/assets/Basic/Wikisearch.png",
+            link: "https://wikili.ccbp.tech/"
+        }, {
+            name: "My first website",
+            image: "/assets/Basic/Ktmm.png",
+            link: "https://ktmm.ccbp.tech/"
+        },
+
     ]
 
     const settings = {
@@ -109,6 +109,9 @@ const Projects = () => {
         slidesToShow: 3,
         slidesToScroll: 1,
         dotsClass: "slick-dots slick-thumb",
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: true,
         appendDots: (dots) => (
             <div style={{ display: "flex", justifyContent: "center" }}>
                 <ul style={{ margin: "0px" }}>{dots.slice(0, 3)}</ul>
@@ -118,10 +121,10 @@ const Projects = () => {
         nextArrow: <button className="slick-next">{">"}</button>,
         responsive: [
             {
-                breakpoint: 768, 
+                breakpoint: 768,
                 settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1, 
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
                 }
             }
         ]
@@ -132,17 +135,17 @@ const Projects = () => {
             <h2 className="section_title">My Projects</h2>
             <span className="section_subtitle">What I have done</span>
             <div className="container">
-          
+
                 <h2 className="project_title_main">HTML,CSS & Js</h2>
                 <Slider {...settings}>
-               
+
                     {basicprojects.map((project, index) => (
-                        <div key={index}  className="slider_container">
+                        <div key={index} className="slider_container">
                             <a href={project.link} target="_blank" rel="noopener noreferrer">
-                              
+
                                 <img src={project.image} alt={project.name} className='project_image' />
                                 <h3 className='project_title'>{project.name}</h3>
-                          
+
                             </a>
                         </div>
                     ))}
@@ -150,7 +153,7 @@ const Projects = () => {
 
                 <h2 className="project_title_main">Froentend Projects</h2>
                 <Slider {...settings}>
-               
+
                     {froentendprojects.map((project, index) => (
                         <div key={index} style={{ padding: '10px' }} className="slider_container">
                             <a href={project.link} target="_blank" rel="noopener noreferrer">
@@ -163,7 +166,7 @@ const Projects = () => {
 
                 <h2 className="project_title_main">Full Stack Projects</h2>
                 <Slider {...settings}>
-               
+
                     {fullstackprojects.map((project, index) => (
                         <div key={index} style={{ padding: '10px' }} className="slider_container">
                             <a href={project.link} target="_blank" rel="noopener noreferrer">
@@ -174,7 +177,7 @@ const Projects = () => {
                     ))}
                 </Slider>
 
-              
+
             </div>
         </section>
     );
